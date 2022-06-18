fae1dde22c98c2f0b0ccebca03001170cd0d6407
Joaquindev92 <a1712802@idat.edu.pe>
2022-06-18 17:21:59 -0500

    Creacion proyecto

---
+++ src/test/java/com/ec2/joaquin/Ec2JoaquinGonzalesServicioBodegaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ec2.joaquin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ec2JoaquinGonzalesServicioBodegaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
